5550a213f09a921845e360d567080e73c03016cb
jerry <614237720@qq.com>
2018-07-08 13:47:12 +0800

    修改cap参数

--- xueqiu/xueqiutest.py
+++ xueqiu/xueqiutest.py
@@ -14,6 +14,12 @@ class xueqiu(unittest.TestCase):
         desired_caps['appActivity'] = '.view.WelcomeActivityAlias'
         desired_caps['dontStopAppOnReset'] = True
         desired_caps['NoReset'] =  True
+
+        # 输入法
+        desired_caps['unicodeKeyboard'] = 'true'  # 支持中文输入，而且不会乱跳
+        desired_caps['resetKeyboard'] = 'true'  # 运行结束以后，删除appium键盘
+        desired_caps['noReset'] = 'true'  # 不做应用清除
+
         # desired_caps['udid'] =   'FA6B4BN00753'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
         self.driver.implicitly_wait(5)
